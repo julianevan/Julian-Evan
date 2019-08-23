3ddc494072654c270500fc27c26c39d3e95bda41
Lindsay Ward <lindsay.ward@jcu.edu.au>
2019-08-23 17:23:22 +1000

    Initial commit

---
+++ assignment1.py
@@ -0,0 +1,12 @@
+"""
+Replace the contents of this module docstring with your own details.
+"""
+
+
+def main():
+    """..."""
+    print("Movies To Watch 1.0 - by <Your Name>")
+
+
+if __name__ == '__main__':
+    main()
