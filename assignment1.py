9012181916968fdecdcd27c3f661e2dff9e00b0f
Julian Evan <julian.evan@my.jcu.edu.au>
2019-11-28 17:12:28 +0800

    try making the menu

--- assignment1.py
+++ assignment1.py
@@ -8,8 +8,31 @@ GitHub URL:
 
 def main():
     """..."""
-    print("Movies To Watch 1.0 - by <Your Name>")
+    print("Movies To Watch 1.0 - by Julian")
+    file = open("movies.csv",'r')
+    print("5 movies loaded")
+    choice = str(input("Menu: \n"
+          "L - List movies \n"
+          "A - Add new movie \n"
+          "W - Watch new movie \n"
+          "Q - Quit"))
+    choice.upper()
+    if choice == "L":
+        while i > 5:
+            for line in file:
+                if 
+                print("{:<10s}")
 
+    elif choice == "A":
+    elif choice == "W":
+    elif choice == "Q":
+    else:
+        print("Invalid input")
+        choice = str(input("Menu: \n"
+                           "L - List movies \n"
+                           "A - Add new movie \n"
+                           "W - Watch new movie \n"
+                           "Q - Quit"))
 
 if __name__ == '__main__':
     main()
